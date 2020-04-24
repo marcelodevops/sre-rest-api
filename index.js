4cd94669d5be6fa14952e5177d3768ed0e0213a4
Marcelo garcia soto <marcelo.algarciass@gmail.com>
2020-04-24 19:48:06 -0400

    added db connection implemetation

--- index.js
+++ index.js
@@ -9,13 +9,28 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 
 const port = process.env.PORT || 8082;
-
+//handler to root directory get request
 app.get('/', async (req, res) => {
     res.json({ status: 'API is ready'});
 });
 
 
 
+let cachedDb;
+function getDbPool() {
+    if(!cachedDb) {
+        cachedDb = mysql.createPool({
+            connectionLimit: 1,
+            user: process.env.SQL_USER,
+            password: process.env.SQL_PASSWORD,
+            database: process.env.SQL_NAME,
+            socketPath: `/cloudsql/${process.env.INST_CON_NAME}`
+
+        });
+        
+    }
+    return cachedDb;
+}
 
 app.listen(port, () => {
   console.log('REST API listening on port ', port);
