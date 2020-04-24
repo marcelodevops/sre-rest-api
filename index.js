babd367d26553ac6a738cfaf330dbe0cfe003c91
Marcelo garcia soto <marcelo.algarciass@gmail.com>
2020-04-24 19:58:43 -0400

    added call to db to get people record by id

--- index.js
+++ index.js
@@ -1,21 +1,31 @@
-//import libraries
-
+//importing express library
 const express = require('express');
+//creating express app
 const app = express();
-
+//importing mysql library
 const mysql = require('mysql');
 
+// importing body parser and link it to the express app
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 
+//port definition, if environment variable is undefined use default to 8082
 const port = process.env.PORT || 8082;
+
 //handler to root directory get request
 app.get('/', async (req, res) => {
     res.json({ status: 'API is ready'});
 });
-
-
-
+//get people instance by id handler
+async function getPeopleById(id) {
+    return new Promise(function(resolve, reject){
+        const sql = 'SELECT * FROM people WHERE id=?';
+        getDbPool().query(sql, [id], (err, results) => {
+            resolve(results[0]);
+        });
+    });
+}
+//db connection function
 let cachedDb;
 function getDbPool() {
     if(!cachedDb) {
@@ -31,7 +41,7 @@ function getDbPool() {
     }
     return cachedDb;
 }
-
+//start listening on defined port
 app.listen(port, () => {
   console.log('REST API listening on port ', port);
 });
